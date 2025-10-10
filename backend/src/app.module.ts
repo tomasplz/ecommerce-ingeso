ae6bdc5764ea107d9b341bd65281c327a46a24cf
tomasplz <tomasplz@hotmail.com>
2025-10-09 22:33:53 -0300

    feat(database): configure TypeORM with PostgreSQL connection settings

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres-db',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
